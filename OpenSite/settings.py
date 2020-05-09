9f09126388f31a8e9f3b7ba8173169a58a44a2c3
Andrew Grant <andrew@grantnet.us>
2020-05-08 20:44:36 -0400

    Abandon MongoDB

--- OpenSite/settings.py
+++ OpenSite/settings.py
@@ -80,12 +80,10 @@ WSGI_APPLICATION = 'OpenSite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-    'default' : { 'ENGINE' : 'djongo', 'NAME' : 'OpenBenchDB' }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
