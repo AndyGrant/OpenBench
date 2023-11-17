bbe7cc25c1ba9d3d3bae9d560ccc50eabfd36c1e
andrew <andrew.github@grantnet.us>
2023-11-16 19:11:57 -0500

    Update cleanup.sh

--- Client/cleanup.sh
+++ Client/cleanup.sh
@@ -1,11 +1,4 @@
-#!/bin/sh
-
-rm -rf __pycache__/
+find -type f ! -name 'client.py' ! -name 'cleanup.sh' -exec rm {} +
 rm -rf Engines/
-rm -rf Networks/
-rm -rf Books/
-rm -rf PGNs/
-rm -rf tmp/
-rm -rf *.dll
-rm -rf cutechess*
-rm -rf machine.txt
+rm -rf PGNS/
+rm -rf __pycache__/
