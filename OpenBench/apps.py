06b4c186acd5ab65937cf1fd032677fcd5a281a7
Andrew Grant <andrew.github@grantnet.us>
2024-12-20 22:41:35 -0500

    WIP: WSGI handling of Watchers + Graceful exits Linux/Windows

--- OpenBench/apps.py
+++ OpenBench/apps.py
@@ -18,7 +18,39 @@
 #                                                                             #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+import atexit
+import os
+import threading
+import platform
+
 import django.apps
+
+LOCKFILE_PATH = 'openbench_watchers.lock'
+IS_WINDOWS    = platform.system() == 'Windows'
+
+def acquire_watcher_lockfile():
+
+    lockfile = None
+
+    try: # Failed to open the file entirely
+        lockfile = open(LOCKFILE_PATH, 'w')
+    except: return None
+
+    try:
+
+        if IS_WINDOWS:
+            import msvcrt
+            msvcrt.locking(lockfile.fileno(), msvcrt.LK_NBLCK, 1)
+
+        else:
+            import fcntl
+            fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+    except: # Failed to acquire the lock, but must still close the file
+        lockfile.close()
+        return None
+
+    return lockfile
 
 
 class OpenBenchConfig(django.apps.AppConfig):
@@ -28,6 +60,45 @@ class OpenBenchConfig(django.apps.AppConfig):
     def ready(self):
 
         from OpenBench import config
+        from OpenBench.watcher import ArtifactWatcher
+        from OpenBench.pgn_watcher import PGNWatcher
 
-        if config.OPENBENCH_CONFIG is None:
-            config.OPENBENCH_CONFIG = config.create_openbench_config()
+        self.lockfile = acquire_watcher_lockfile()
+
+        if self.lockfile:
+
+            # Load all of the .json config files
+            if config.OPENBENCH_CONFIG is None:
+                config.OPENBENCH_CONFIG = config.create_openbench_config()
+
+            # Signals to stop the watchers
+            self.stop_artifact_watcher = threading.Event()
+            self.stop_pgn_watcher      = threading.Event()
+
+            # Each watcher is a threading.Thread
+            self.artifact_watcher = ArtifactWatcher(self.stop_artifact_watcher, daemon=True)
+            self.pgn_watcher      = PGNWatcher(self.stop_pgn_watcher, daemon=True)
+
+            # Start everything
+            self.artifact_watcher.start()
+            self.pgn_watcher.start()
+
+            # Ensure we cleanup upon exit
+            atexit.register(self.shutdown)
+
+    def shutdown(self):
+
+        # Signal the Artifact Watcher to shutdown
+        if hasattr(self, 'artifact_watcher') and self.artifact_watcher.is_alive():
+            self.stop_artifact_watcher.set()
+            self.artifact_watcher.join()
+
+        # Signal the PGN Watcher to shutdown
+        if hasattr(self, 'pgn_watcher') and self.pgn_watcher.is_alive():
+            self.stop_pgn_watcher.set()
+            self.pgn_watcher.join()
+
+        # Cleanup Lockfile if we hold it
+        if self.lockfile:
+            self.lockfile.close()
+            os.remove(LOCKFILE_PATH)
