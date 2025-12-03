73c45396d64dd5b2b53a2b0f1bbbaa790b37ed9f
Andrew Grant <andrew.github@grantnet.us>
2025-12-03 00:01:25 -0500

    Stash

--- OpenBench/spsa_utils.py
+++ OpenBench/spsa_utils.py
@@ -18,6 +18,33 @@
 #                                                                             #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+import numpy as np
+
+def spsa_param_digest_headers(workload):
+    return ['Name', 'Curr', 'Start', 'Min', 'Max', 'C', 'C_end', 'R', 'R_end']
+
+def spsa_original_input(workload):
+
+    lines = []
+    for param in workload.spsa_run.parameters.order_by('index'):
+        lines.append(', '.join([
+            param.name,
+            'float' if param.is_float else 'int',
+            str(param.start),
+            str(param.min_value),
+            str(param.max_value),
+            str(param.c_end),
+            str(param.r_end),
+        ]))
+
+    return '\n'.join(lines)
+
+def spsa_optimal_values(workload):
+    return '\n'.join([
+        '%s, %s' % (param.name, param.value if param.is_float else int(round(param.value)))
+        for param in workload.spsa_run.parameters.order_by('index')
+    ])
+
 def spsa_param_digest(workload):
 
     # C & R, as if we were being assigned a workload right now
@@ -48,28 +75,26 @@ def spsa_param_digest(workload):
 
     return digest
 
-def spsa_param_digest_headers(workload):
-    return ['Name', 'Curr', 'Start', 'Min', 'Max', 'C', 'C_end', 'R', 'R_end']
+def spsa_workload_assignment_dict(workload, data):
 
-def spsa_original_input(workload):
+    params = list(workload.spsa_run.parameters.order_by('index'))
 
-    lines = []
-    for param in workload.spsa_run.parameters.order_by('index'):
-        lines.append(', '.join([
-            param.name,
-            'float' if param.is_float else 'int',
-            str(param.start),
-            str(param.min_value),
-            str(param.max_value),
-            str(param.c_end),
-            str(param.r_end),
-        ]))
+    names  = [p.name for p in params]
+    values = np.array([p.value     for p in params])
+    mins   = np.array([p.min_value for p in params])
+    maxs   = np.array([p.max_value for p in params])
+    c_ends = np.array([p.c_end     for p in params])
+    r_ends = np.array([p.r_end     for p in params])
 
-    return '\n'.join(lines)
+    # Only use one set of parameters if distribution is SINGLE.
+    # Duplicate the params, even though they are the same, across all
+    # sockets on the machine, in the event of a singular SPSA distribution
 
-def spsa_optimal_values(workload):
+    is_single    = test.spsa['distribution_type'] == 'SINGLE'
+    permutations = 1 if is_single else workload['distribution']['cutechess-count']
+    duplicates   = 1 if not is_single else workload['distribution']['cutechess-count']
 
-    return '\n'.join([
-        '%s, %f' % (param.name, param.value if param.is_float else round(param.value))
-        for param in workload.spsa_run.parameters.order_by('index')
-    ])
+    # C & R are scaled over the course of the iterations
+    iteration     = 1 + (test.games / (test.spsa['pairs_per'] * 2))
+    c_compression = iteration ** test.spsa['Gamma']
+    r_compression = (test.spsa['A'] + iteration) ** test.spsa['Alpha']
