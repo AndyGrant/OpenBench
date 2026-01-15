9a3a31761baad71cb5e8300c5d458539ff172751
Andrew Grant <andrew.github@grantnet.us>
2026-01-15 04:55:38 -0500

    Kick Client/Server to v40

--- Client/worker.py
+++ Client/worker.py
@@ -59,7 +59,7 @@ from client import try_forever
 
 ## Basic configuration of the Client. These timeouts can be changed at will
 
-CLIENT_VERSION   = 39 # Client version to send to the Server
+CLIENT_VERSION   = 40 # Client version to send to the Server
 TIMEOUT_HTTP     = 30 # Timeout in seconds for HTTP requests
 TIMEOUT_ERROR    = 10 # Timeout in seconds when any errors are thrown
 TIMEOUT_WORKLOAD = 30 # Timeout in seconds between workload requests
